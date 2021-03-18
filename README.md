38ed5549f80ea86ab910aa7132a8dbfe45418356
Satyam54784 <satyamdaga01@gmail.com>
2021-03-19 00:47:06 +0530

    First commit

--- README.md
+++ README.md
@@ -2,3 +2,5 @@
 Q2 of softlab assignment
 This is a readme for this demo repository for the softlab assignment.
 This is about the selesction sort algorithm file uploaded on this demo repository.
+The local repository has been modified and will be uploaded to github
+
--- selection_sort.c
+++ selection_sort.c
@@ -38,7 +38,7 @@ void printArray(int arr[], int size)
 // Driver program to test above functions 
 int main() 
 { 
-	int arr[] = {64, 25, 12, 22, 11}; 
+	int arr[] = {612, 5225, 112, 222, 11}; 
 	int n = sizeof(arr)/sizeof(arr[0]); 
 	selectionSort(arr, n); 
 	printf("Sorted array: \n"); 
