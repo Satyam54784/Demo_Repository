8f73d9a7d8898138d07af6a7d9b0bd09a96194cb
Satyam54784 <80911005+Satyam54784@users.noreply.github.com>
2021-03-19 00:51:32 +0530

    Update selection_sort.c

--- selection_sort.c
+++ selection_sort.c
@@ -38,7 +38,7 @@ void printArray(int arr[], int size)
 // Driver program to test above functions 
 int main() 
 { 
-	int arr[] = {612, 5225, 112, 222, 11}; 
+	int arr[] = {612, 52, 112, 222, 11}; 
 	int n = sizeof(arr)/sizeof(arr[0]); 
 	selectionSort(arr, n); 
 	printf("Sorted array: \n"); 
